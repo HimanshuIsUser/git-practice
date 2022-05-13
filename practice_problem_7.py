c3320a92597e9631527ae6ddd2713b5c8eae2b2a
Himanshu Bansal <himanshub166@gmail,com>
2022-05-13 19:48:15 +0700

    13 may

--- practice_problem_7.py
+++ practice_problem_7.py
@@ -1,9 +1,46 @@
-import re
+def funt(string1,string2):
+    word1 = string1.split(" ")
+    word2 = string2.split(" ")
+    score = 0
+    for words1 in word1:
+        for words2 in word2:
+            # print(f'{words1} mathcing with {words2}')
+            if words1.lower()==words2.lower():
+                score = score+1
+    return score
+if __name__ == '__main__':
+    # funt("this is harry","this is himanshu")
+    sentences = ["this is harry","my name is himanshu","this one is gourav","this one is manish"]
+    query = input("enter any string : ")
+    score = [funt(query,sentence)for sentence in sentences]
+    hard = [hards for hards in sorted(zip(score,sentences)) if hards[0]!=0]
+    hard.sort(reverse=True)
+    print(f'{len(hard)} returns match')
+    for score ,item in hard:
+        print(f'{item} with {score}')
 
-lst = ["My dreams come true when I have one car","when i have my own house","when i can take care of my lady","when i can traller all over india without thinking about my expens","when i dont need to take care about my children's future","when i can live as a legend","most important when i can die peacefully","afte that when i meet to my lovely super ,beautyfull lady my mom"]
-t = ' '.join(lst)
-print(t)
-j = re.compile( )
-p = j.finditer(str(t))
-for item in p:
-    print(item)
+
+
+
+#
+# def funt(string1,string2):
+#     words1 = string1.strip().split(" ")
+#     words2 = string2.strip().split(" ")
+#     score = 0
+#     for word1 in words1:
+#         for word2 in words2:
+#             if word1.lower()==word2.lower():
+#                 score+=1
+#     return score
+#
+# if __name__ == '__main__':
+#     # print(funt("this is harry","is a himanshu"))
+#     sentence = ["this is raghav","manoj is loyal","himanshu is a judge"]
+#     que = input("enter your query : ")
+#     hard = [funt(que,sentences)for sentences in sentence]
+#     print(hard)
+#     new = [hards for hards in sorted(zip(hard,sentence))if hards[0]!=0]
+#     print(new)
+#     print(f'{len(new)} results found')
+#     for hard,item in new:
+#         print(f'\"{que}\" found in \"{item}\" with {hard}')
---
+++ practice_problem_8.py
@@ -0,0 +1,29 @@
+import random
+def rohanmultiplication(t):
+    s = []
+    n = random.randint(2,11)
+    for i in range(1,11):
+        j = random.randint(2,10)
+        if(i==n):
+            f = t*i+j
+        else:
+            f = t*i
+        s.append(f)
+    print(f'{s} this is rohan\'s programme')
+    k = 0
+    lst = []
+    for i in s:
+        k = k+1
+        o = t*k
+        if (i==o):
+            lst.append(i)
+        else:
+            lst.append(o)
+    print(f'{lst} real')
+    if(s==lst):
+        print(f'both are equal :)')
+    else:
+        print(f'Error!\ncheck the list for confirmation :( ')
+if __name__ == '__main__':
+    t = int(input("enter the value : "))
+    rohanmultiplication(t)
